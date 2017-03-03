3046c3dd77267421b56a3a44a4e3458f4261b2a6
zhancongc <1227753320@qq.com>
2017-03-03 15:38:01 +0800

    fix a bug of no article no index

--- config.py
+++ config.py
@@ -14,20 +14,20 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 # track modifications is suggested opening
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 # uri of database
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:security@localhost:3306/blog?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:security@localhost:3306/blog?charset=utf8mb4'
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
 # mail server,port,username,password of sender
-MAIL_SERVER = 'smtp.qq.com'
-MAIL_PORT = 25
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or '1227753320@qq.com'
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'zaq1234567890okm'
+MAIL_SERVER = 'smtp.mxhichina.com'
+MAIL_PORT = 465
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'zhancongc@lightblog.site'
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'Zcc940822'
 
 # whether to use tls and ssl
-MAIL_USE_TLS = True
-MAIL_USE_SSL = False
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
 
 # the subject of mail
 FLASK_MAIL_SUBJECT_PREFIX = 'Light Blog'
@@ -36,8 +36,10 @@ FLASK_MAIL_SUBJECT_PREFIX = 'Light Blog'
 MAIL_DEBUG = True
 
 # articles displayed per page
-FLASK_ARTICLE_PER_PAGE = 10
+FLASK_ARTICLE_PER_PAGE = 5
 
 # followers or followed_by displayed per page
-FLASK_FOLLOW_PER_PAGE = 10
+FLASK_FOLLOW_PER_PAGE = 20
 
+# comments' number one page show
+FLASK_COMMENTS_PER_PAGE = 10
